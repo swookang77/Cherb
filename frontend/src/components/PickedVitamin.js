3b0a6f9ce4f27ada48b884af773981160d9283a3
Kang sungwoo <106954289+midnightkang@users.noreply.github.com>
2023-07-04 23:23:13 +0900

    [FEAT]PickedVitamin Component 초기화 버튼 누르면 목록 초기화

--- frontend/src/components/PickedVitamin.js
+++ frontend/src/components/PickedVitamin.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ListGroup from "react-bootstrap/ListGroup";
 import { useSelector } from "react-redux";
+import { persistor } from "../store";
 
 export default function PickedVitamin() {
   const vitaminList = useSelector((state) => state.vitaminList.data);
-
+  //초기화 버튼.
+  const handleReset = () =>{
+    persistor.purge();
+    window.location.reload();
+  }
   return (
     <div>
       영양제 리스트
@@ -12,6 +17,7 @@ export default function PickedVitamin() {
         {vitaminList.map((item, index) => (
           <ListGroup.Item key={index}>{item.title}</ListGroup.Item>
         ))}
+        <button onClick={handleReset}>초기화</button>
       </ListGroup>
     </div>
   );
